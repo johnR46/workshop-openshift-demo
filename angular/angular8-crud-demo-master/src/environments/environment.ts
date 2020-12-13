ebac07d46a6e8360b000a0dfb47492a1d4630da7
john-3g-dev <machine.the@hotmail.com>
2020-12-13 19:14:09 +0700

    change problem nginx

--- angular/angular8-crud-demo-master/src/environments/environment.ts
+++ angular/angular8-crud-demo-master/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //baseUrl: 'http://localhost:8080'
-  baseUrl: 'http://192.168.64.4:30163'
+  baseUrl: ' http://crud-api-v2-workshop-john5.apps-crc.testing'
 
 };
 
